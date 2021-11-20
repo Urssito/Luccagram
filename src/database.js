4a47d5de6a0c50780f1d406357dd899db3dee7ba
urssito <lucca.urso112@gmail.com>
2021-11-19 21:56:06 -0300

    fixing database script

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongo = require("mongoose");
 
-uri = "mongodb://localhost:27017/lg-db-app";
+uri = "mongodb+srv://urssito:<password>@cluster0.vosld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongo.connect(uri)
   .then(db => console.log("DB connected in: ", uri))
